d9681314f82bcd9b304c8047dc95563369b34418
Ryou <ryoutatsukiii@gmail.com>
2026-09-18 19:37:57 +0800

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Manila');
-$con = mysqli_connect("localhost","root","","gym_signup");
-
-if(!$con){
-    die("Connection Failed: ".mysqli_connect_error());
-}
-?>
